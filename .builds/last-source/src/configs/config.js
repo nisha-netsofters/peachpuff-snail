b4525d8ce0545e664b8185d4524fb2ecd1008dc3
kevalnetsofters <hetvi@netsofters.com>
2026-07-25 10:26:38 +0530

    live url update

--- .builds/last-source/src/configs/config.js
+++ .builds/last-source/src/configs/config.js
@@ -1,7 +1,7 @@
 // export const SERVER_URL = "http://192.168.29.5:7000/api/"  //"local host"
 // export const SERVER_URL = "https://koqcfsplcf3kiudmuxezmpvrdi0luyjo.lambda-url.ap-south-1.on.aws/api/"  //"Development"
-export const SERVER_URL = "http://localhost:7001/api/"  // local backend
-// export const SERVER_URL = "https://steelblue-goshawk-113691.hostingersite.com/api/"  // production
+// export const SERVER_URL = "http://localhost:7001/api/"  // local backend
+export const SERVER_URL = "https://steelblue-goshawk-113691.hostingersite.com/api/"  // production
 // export const SERVER_URL = "https://koqcfsplcf3kiudmuxezmpvrdi0luyjo.lambda-url.ap-south-1.on.aws/api/"  //"Development"
 // export const SERVER_URL = "https://ga5rpjecp76dvmpztnkpgexvoa0tbcnt.lambda-url.us-east-1.on.aws/api"  // "Production"
 // export const IMAGE_URL = "https://koqcfsplcf3kiudmuxezmpvrdi0luyjo.lambda-url.ap-south-1.on.aws/"
